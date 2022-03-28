c6967999a734297f19ad568acda735e2d34ed99c
Sandi Ibraimoff <sam.ibraimoff@gmail.com>
2022-03-28 15:39:59 -0400

    context and reducer added

--- src/context/context.js
+++ src/context/context.js
@@ -1,10 +1,58 @@
-import { createContext } from 'react';
+import { createContext, useReducer } from 'react';
+import { reducer } from './reducer';
 
 export const ShopContext = createContext();
 
+const initialState = {
+  goods: [],
+  loading: true,
+  order: [],
+  isBasketShow: false,
+  alertName: '',
+};
+
 export const ContextProvider = ({ children }) => {
-  const value = {
-    goods: 'text goods',
+  const [value, dispatch] = useReducer(reducer, initialState);
+
+  value.addToBasket = (item) => {
+    dispatch({
+      type: 'ADD_TO_BASKET',
+      payload: item,
+    });
   };
+
+  value.increaseOrderQuantity = (id) => {
+    dispatch({
+      type: 'INCREASE_QUANTITY',
+      payload: id,
+    });
+  };
+
+  value.decreaseOrderQuantity = (id) => {
+    dispatch({
+      type: 'DECREASE_QUANTITY',
+      payload: id,
+    });
+  };
+
+  value.closeAlert = () => {
+    dispatch({
+      type: 'CLOSE_ALERT',
+    });
+  };
+
+  value.removeFromBasket = (itemId) => {
+    dispatch({
+      type: 'REMOVE_FROM_BASKET',
+      payload: { id: itemId },
+    });
+  };
+
+  value.handleBasketShow = () => {
+    dispatch({
+      type: 'BASKET_TOGGLE',
+    });
+  };
+
   return <ShopContext.Provider value={value}>{children}</ShopContext.Provider>;
 };
